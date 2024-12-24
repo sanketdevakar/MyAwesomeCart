aace9c88809d60c0a0300f1b9889a9b314877cf2
sanketdevakar <sanketdevakar09@gmail.com>
2024-12-24 13:01:10 +0530

    Created Product database

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("search/", views.search, name="Search"),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("productview/", views.productView, name="ProductView"),
+    path("checkout/", views.checkout, name="Checkout"),
+
 ]

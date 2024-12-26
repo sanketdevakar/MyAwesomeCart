dd09a774c2758ae4fa3bcbf6c2241d7315f24262
sanketdevakar <sanketdevakar09@gmail.com>
2024-12-26 23:52:30 +0530

    Added new html file to display product's list

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 # Create your views here.
+def ProductList(request):
+    context  = {
+        'product':Product.objects.all()
+    }
+    return render(request,'shop/product.html',context)
+
 def index(request):
     return render(request, 'shop/index.html')
 

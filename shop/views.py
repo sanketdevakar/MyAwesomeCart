08a55a5868af9200322e7dde4d480988fc6e7ed7
sanketdevakar <sanketdevakar09@gmail.com>
2024-12-31 11:36:42 +0530

    Logic written to display category wise product on website

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from math import ceil
 
 
 # Create your views here.
@@ -11,7 +12,20 @@ def ProductList(request):
     return render(request,'shop/product.html',context)
 
 def index(request):
-    return render(request, 'shop/index.html')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return HttpResponse("We are at about")

68b0668893565337a4fa2414f7d96e2492c811ec
sanketdevakar <sanketdevakar09@gmail.com>
2025-01-02 23:27:30 +0530

    Created Contact page and database to store the user's details

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Contact
 from math import ceil
 
 
@@ -27,7 +27,16 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return render(request, 'shop/contact.html')
+    if request.method=="POST":
+        print(request)
+        name=request.POST.get('name', '')
+        email=request.POST.get('email', '')
+        phone=request.POST.get('phone', '')
+        desc=request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+    return render(request, "shop/contact.html")
+
 
 def tracker(request):
     return render(request, 'shop/tracker.html')

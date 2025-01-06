d102b18dc6c36dbd793242c1ef6e0c1d48cada24
sanketdevakar <sanketdevakar09@gmail.com>
2025-01-07 00:10:01 +0530

    Added search functionality

--- shop/views.py
+++ shop/views.py
@@ -53,6 +53,31 @@ def checkout(request):
         return render(request, 'shop/checkout.html', {'thank':thank, 'id':id})
     return render(request, 'shop/checkout.html')
 
+def searchMatch(query, item):
+    '''return true only if query matches the item'''
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query = request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod) != 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, "msg": ""}
+    if len(allProds) == 0 or len(query)<4:
+        params = {'msg': "Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
+
 
 def tracker(request):
     if request.method=="POST":
@@ -74,8 +99,6 @@ def tracker(request):
 
     return render(request, 'shop/tracker.html')
 
-def search(request):
-    return render(request, 'shop/search.html')
 
 def productView(request, myid):
     # Fetch the product using the id
